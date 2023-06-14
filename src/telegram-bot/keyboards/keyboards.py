1855e26a9f035a6fbaa339bc3f7107bd3ffe0b8f
Shinkuvichh <112030252+Shinkuvichh@users.noreply.github.com>
2023-06-14 17:31:00 +0300

    Update keyboards.py

--- src/telegram-bot/keyboards/keyboards.py
+++ src/telegram-bot/keyboards/keyboards.py
@@ -1,56 +1,85 @@
-from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup, KeyboardButton
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
+# общее
+continue_button = InlineKeyboardButton(text="Продолжить", callback_data="continue")
+# БЭКНУТЬ БЭК
+back_button = InlineKeyboardButton(text="На прошлый шаг", callback_data="back")
+back_board = InlineKeyboardMarkup()
+back_board.add(back_button)
 
 # main menu
-menu_board = InlineKeyboardMarkup(resize_keyboard=True)
+menu_board = InlineKeyboardMarkup()
 forget_button = InlineKeyboardButton(text="Удалить анкету", callback_data="forget")
 check_button = InlineKeyboardButton(text="Посмотреть вакансии", callback_data="check")
 menu_board.add(forget_button, check_button)
 
 # delete notification
 delete_button = InlineKeyboardButton(text="Скрыть это сообщение", callback_data="delete")
-delete_board = InlineKeyboardMarkup(resize_keyboard=True)
+delete_board = InlineKeyboardMarkup()
 delete_board.add(delete_button)
 
 # register
-register_board = InlineKeyboardMarkup(resize_keyboard=True)
+register_board = InlineKeyboardMarkup()
 register_button = InlineKeyboardButton(text="Заполнить анкету", callback_data="register")
 register_board.add(register_button)
 
 # remote?
-remote_board = ReplyKeyboardMarkup(resize_keyboard=True)
-remote_button = KeyboardButton(text="Хочу только удалённо")
+remote_board = InlineKeyboardMarkup()
+remote_button = InlineKeyboardButton(text="Только удалённо", callback_data="remote_only")
 remote_board.add(remote_button)
 
-# grade
-grade_board = ReplyKeyboardMarkup(resize_keyboard=True)
-gradej_button = KeyboardButton(text="Junior")
-gradem_button = KeyboardButton(text="Middle")
-grades_button = KeyboardButton(text="Senior")
-gradel_button = KeyboardButton(text="Lead")
-grade_board.add(gradej_button, gradem_button, grades_button, gradel_button)
-
 # yes\no
-yesno_board = ReplyKeyboardMarkup(resize_keyboard=True)
-yes_button = KeyboardButton(text="Да")
-no_button = KeyboardButton(text="Нет")
-yesno_board.add(yes_button, no_button)
+yesno_board = InlineKeyboardMarkup()
+yes_button = InlineKeyboardButton(text="Да", callback_data="yes")
+no_button = InlineKeyboardButton(text="Нет", callback_data="no")
+yesno_board.row(yes_button, no_button)
+yesno_board.row(back_button)
 
 # skills
-skills_board = InlineKeyboardMarkup(resize_keyboard=True, row_width=3)
-java_button = InlineKeyboardButton(text="Java", callback_data="Java")
-javascript_button = InlineKeyboardButton(text="JavaScript", callback_data="JavaScript")
-sql_button = InlineKeyboardButton(text="SQL", callback_data="SQL")
-cpp_button = InlineKeyboardButton(text="C++", callback_data="C++")
-csharp_button = InlineKeyboardButton(text="C#", callback_data="C#")
-c_button = InlineKeyboardButton(text="C", callback_data="C")
-net_button = InlineKeyboardButton(text=".NET", callback_data=".NET")
-python_button = InlineKeyboardButton(text="Python", callback_data="Python")
-rust_button = InlineKeyboardButton(text="Rust", callback_data="Rust")
-golang_button = InlineKeyboardButton(text="Golang", callback_data="Golang")
-swift_button = InlineKeyboardButton(text="Swift", callback_data="Swift")
-continue_button = InlineKeyboardButton(text="Продолжить", callback_data="askforsalary")
-skills_available = [java_button, javascript_button, sql_button, cpp_button, csharp_button, c_button, net_button,
-                    python_button, rust_button, golang_button, swift_button]
-for skill in skills_available:
-    skills_board.add(skill)
-skills_board.add(continue_button)
+dev_skills_available = ["FrontEnd", "Node.js", "GameDev", "Embedded", "1C", "DB", "Fullstack", "C#", "C++", "Golang",
+                        "Desktop",
+                        "Mobile", "Java", "PHP", "Python", "Ruby", "Scala"]
+other_skills_available = ["Devops", "QA", "CTO", "DA/DBA", "Architect", "Data engineer", "Data science",
+                          "Data Analyst", "Техподдержка", "Технический писатель",
+                          "Cистемный администратор",
+                          "Cистемная аналитика", "Бизнес аналитика",
+                          "Продуктовая аналитика", "Информационная безопасность"]
+
+grades_available = ["Junior", "Middle", "Senior", "Lead"]
+skill_next_button = InlineKeyboardButton(text="▶", callback_data="switch_board")
+skill_back_button = InlineKeyboardButton(text="◀", callback_data="switch_board")
+
+
+# динамическая клавиатура
+async def grade_board(chosen: set[str]) -> InlineKeyboardMarkup:
+    new_board = InlineKeyboardMarkup()
+    for variant in grades_available:
+        if variant in chosen:
+            new_board.add(
+                InlineKeyboardButton(text=f"{variant} ☑️", callback_data=variant))
+        else:
+            new_board.add(InlineKeyboardButton(text=variant, callback_data=variant))
+    new_board.row(back_button, continue_button)
+    return new_board
+
+
+async def skills_board(chosen: set[str]) -> InlineKeyboardMarkup:
+    if "switch_board" not in chosen:
+        new_board = InlineKeyboardMarkup(row_width=3)
+        for variant in dev_skills_available:
+            if variant in chosen:
+                new_board.insert(
+                    InlineKeyboardButton(text=f"{variant} ☑️", callback_data=variant))
+            else:
+                new_board.insert(InlineKeyboardButton(text=variant, callback_data=variant))
+        new_board.row(back_button, skill_next_button)
+    else:
+        new_board = InlineKeyboardMarkup(row_width=2)
+        for variant in other_skills_available:
+            if variant in chosen:
+                new_board.insert(
+                    InlineKeyboardButton(text=f"{variant} ☑️", callback_data=variant))
+            else:
+                new_board.insert(InlineKeyboardButton(text=variant, callback_data=variant))
+        new_board.row(skill_back_button, continue_button)
+    return new_board
